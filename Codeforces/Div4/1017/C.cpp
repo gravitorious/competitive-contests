29bed4b2140a0dabaab93f0b1dda810813acf6b6
gravitorious <gravitorious98@gmail.com>
2025-04-14 17:53:12 +0300

    Div4-1017

---
+++ Codeforces/Div4/1017/C.cpp
@@ -0,0 +1,95 @@
+////created by gravitorious
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#ifdef XOX
+#include "../../../debug.h"
+#else
+#define debug(...) 77
+#endif
+
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+ 
+#define getunique(v) {sort(v.begin(), v.end()); v.erase(unique(v.begin(), v.end()), v.end());}
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+template <class T>
+void chkmax(T &x,T y){
+    if(x < y) x = y;
+}
+ 
+template <class T>
+void chkmin(T &x,T y){
+    if(x > y) x = y;
+}
+ 
+constexpr int popcnt(int x){
+    return __builtin_popcount(x);
+}
+ 
+constexpr int ctz(int x){
+    return __builtin_ctz(x);
+}
+
+constexpr double int_part(double x, double *intpart){ 
+	return modf(x, intpart); //returns the real part
+}
+ 
+ll ceil_div(ll a, ll b) {
+	return a / b + ((a ^ b) > 0 && a % b);
+}  // divide a by b rounded up
+ll floor_div(ll a, ll b) {
+	return a / b - ((a ^ b) < 0 && a % b);
+}  // divide a by b rounded down
+ 
+long long myRand(long long B) {
+	//0 to B-1
+	return (unsigned long long)rng() % B;
+}
+ 
+int myUniRand(int a, int b){
+	//a to b
+	uniform_int_distribution<int> distribution(a,b);
+	return distribution(rng);
+}
+ 
+void solve(){
+	int n;
+    cin >> n;
+    vector<int> v(2 * n + 1);
+    vector<bool> c(2 * n + 1, false);
+    for(int i = 1; i <= n; i++){
+    	for(int j = 1; j <= n; j++){
+    		int a;
+    		cin >> a;
+    		v[i + j] = a;
+    		c[a] = true;	
+    	}	
+    }
+    int first;
+    for(int i = 1; i <= 2 * n; i++){
+    	if(c[i] == false){
+    		first = i;
+    	}	
+    }
+    v[1] = first;
+    for(int i = 1; i <= 2 * n; i++){
+    	cout << v[i] << ' ';
+    }
+    cout << '\n';
+}
+ 
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	int tc;
+	cin >> tc;
+	while(tc--) solve();
+	return 0;
+}
---
+++ Codeforces/Div4/1017/D.cpp
@@ -0,0 +1,121 @@
+////created by gravitorious
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#ifdef XOX
+#include "../../../debug.h"
+#else
+#define debug(...) 77
+#endif
+ 
+using ll = long long;
+using ld = long double;
+using uint = unsigned int;
+using ull = unsigned long long;
+ 
+#define getunique(v) {sort(v.begin(), v.end()); v.erase(unique(v.begin(), v.end()), v.end());}
+ 
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+ 
+template <class T>
+void chkmax(T &x,T y){
+    if(x < y) x = y;
+}
+ 
+template <class T>
+void chkmin(T &x,T y){
+    if(x > y) x = y;
+}
+ 
+constexpr int popcnt(int x){
+    return __builtin_popcount(x);
+}
+ 
+constexpr int ctz(int x){
+    return __builtin_ctz(x);
+}
+ 
+constexpr double int_part(double x, double *intpart){ 
+	return modf(x, intpart); //returns the real part
+}
+ 
+ll ceil_div(ll a, ll b) {
+	return a / b + ((a ^ b) > 0 && a % b);
+}  // divide a by b rounded up
+ll floor_div(ll a, ll b) {
+	return a / b - ((a ^ b) < 0 && a % b);
+}  // divide a by b rounded down
+ 
+long long myRand(long long B) {
+	//0 to B-1
+	return (unsigned long long)rng() % B;
+}
+ 
+int myUniRand(int a, int b){
+	//a to b
+	uniform_int_distribution<int> distribution(a,b);
+	return distribution(rng);
+}
+ 
+void solve(){
+	string p, s;
+	cin >> p >> s;
+	int n = (int)p.size();
+	int m = (int)s.size();
+	int same = 1;
+	bool flag = false;
+	int i = 0, j = 0;
+	for(int i = 1 ; i < n; i++){
+		if(p[i] == p[i - 1]){
+			same++;
+			if(i != n - 1){
+				continue;
+			}
+			else flag = true;
+		}
+		int count = 0;
+		for(int z = j; z < m; z++){
+			if(s[z] == p[i - 1]){
+				if(z == m - 1) j = m;
+				count++;
+			}
+			else{
+				j = z;
+				break;
+			}
+		}
+		if(count >= same && count <= 2 * same){
+			same = 1;
+			continue;
+		}
+		else{
+			cout << "NO\n";
+			return;
+		}
+	}
+	if(flag){
+		if(j < m) cout << "NO\n";
+		else cout << "YES\n";
+	}
+	else{
+		int count = 0;
+		for(j; j < m; j++){
+			if(s[j] == p[n - 1]) count++;
+			else break;
+		}
+		if((count == 1 || count == 2) && j == m) cout << "YES\n";
+		else cout << "NO\n";
+	}
+	
+}
+ 
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	int tc;
+	cin >> tc;
+	while(tc--) solve();
+	return 0;
+}
